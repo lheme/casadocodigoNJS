83c2b6ee9d9d8ed85a826fc155fcb267574aa463
Luiz Henrique Leme <lheme@outlook.com.br>
2017-08-26 12:59:14 -0300

    Implementado conexao com banco de dados \n Alterada a pagina para exibir os dados do banco'

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,5 +1,18 @@
 module.exports = function(app) {
     app.get("/produtos",function(req, res) {
-        res.render('produtos/lista');
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host: "localhost",
+            user: "root",
+            password : "root",
+            database: "casadocodigo_nodejs",
+        });
+
+        connection.query('select * from livros', function(err, results){
+            res.render('produtos/lista', {lista:results});
+        });
+
+        connection.end();
+
     });
 }
