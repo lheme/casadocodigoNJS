0025fcce52a640711120f7e612b2c5515319a6df
Luiz Henrique Leme <lheme@outlook.com.br>
2017-09-02 14:40:31 -0300

    Criada a funcao de insert

--- app/infra/ProdutosDAO.js
+++ app/infra/ProdutosDAO.js
@@ -3,7 +3,11 @@ function ProdutosDAO(connection) {
 }
 
 ProdutosDAO.prototype.lista = function(callback) {
-    this._connection.query('select * from livros',callback);
+    this._connection.query('SELECT * FROM livros',callback);
+}
+
+ProdutosDAO.prototype.salva = function(produto,callback) {
+    this._connection.query('INSERT INTO livros SET ?',produto, callback);
 }
 
 module.exports = function(){
