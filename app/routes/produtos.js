491449205bde9876d907e512fa77562b5809460d
Luiz Henrique Leme <lheme@outlook.com.br>
2017-09-03 11:32:26 -0300

    Implementado o content negotiation na aplicacao

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,29 +1,50 @@
 module.exports = function(app) {
-    app.get('/produtos', function(req, res) {
+
+    var listaProdutos = function(req, res) {
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
 
         produtosDAO.lista(function(err, results) {
-            res.render('produtos/lista', {lista: results});
+            res.format({
+                html: function() {
+                    res.render('produtos/lista', {lista:results});
+                },
+                json: function() {
+                    res.json(results);
+                }
+            });
+        });
+
+        connection.end();
+    }
+
+    app.get('/produtos', listaProdutos);
+
+    app.get('/produtos/json', function(req, res) {
+        var connection = app.infra.connectionFactory();
+        var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+        produtosDAO.lista(function(err, results) {
+            res.json(results);
         });
 
         connection.end();
     });
 
-    app.get('/produtos/form', function(req, res){
+    app.get('/produtos/form', function(req, res) {
         res.render('produtos/form');
     });
 
-    app.post('/produtos/salva', function(req, res){
+    app.post('/produtos', function(req, res) {
         var produto = req.body;
-        console.log(produto);
 
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
 
-        produtosDAO.salva(produto, function(erros, resultado){
-            res.render('produtos/lista');
+        produtosDAO.salva(produto, function(err, results) {
+            res.redirect('/produtos');
         });
+
         connection.end();
     });
 }
--- config/express.js
+++ config/express.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var load = require('express-load');
-
 var bodyParser = require('body-parser');
 
 module.exports = function() {
@@ -10,6 +9,7 @@ module.exports = function() {
     app.set('views', './app/views');
 
     app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
 
     load('routes', {cwd: 'app'})
         .then('infra')
