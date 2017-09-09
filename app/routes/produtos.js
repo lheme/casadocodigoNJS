c15eadbac124565a23a1208b8be13d288b93d625
Luiz Henrique Leme <lheme@outlook.com.br>
2017-09-09 12:56:21 -0300

    Implementado a validacao nos campos e apresentacao de mensagem na tela

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -32,14 +32,31 @@ module.exports = function(app) {
     });
 
     app.get('/produtos/form', function(req, res) {
-        res.render('produtos/form');
+        res.render('produtos/form',{errosValidacao:{}, produto:{}});
     });
 
     app.post('/produtos', function(req, res) {
         var produto = req.body;
+        console.log(produto);
 
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
+
+        var validadorTitulo = req.assert('titulo','Titulo deve ser preenchido').notEmpty();
+        req.assert('preco','Preço deve ser um número').isFloat();
+
+        var errors = req.validationErrors();
+        if(errors) {
+            res.format({
+                html: function() {
+                    res.status(400).render('produtos/form',{errosValidacao : errors, produto : produto});
+                },
+                json: function() {
+                    res.status(400).send(errors);
+                }
+            });
+            return;
+        }
 
         produtosDAO.salva(produto, function(err, results) {
             res.redirect('/produtos');
--- config/express.js
+++ config/express.js
@@ -2,6 +2,8 @@ var express = require('express');
 var load = require('express-load');
 var bodyParser = require('body-parser');
 
+var expressValidator = require('express-validator');
+
 module.exports = function() {
     var app = express();
 
@@ -10,6 +12,8 @@ module.exports = function() {
 
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
+
+    app.use(expressValidator());
 
     load('routes', {cwd: 'app'})
         .then('infra')
