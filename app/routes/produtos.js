0095d444b604ee0db2735c3b9a0978841832ac3d
Luiz Henrique Leme - lheme <lheme@outlook.com.br>
2017-10-13 14:12:25 -0300

    Inclusao de testes

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,10 +1,13 @@
 module.exports = function(app) {
 
-    var listaProdutos = function(req, res) {
+    var listaProdutos = function(req, res, next) {
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
 
         produtosDAO.lista(function(err, results) {
+            if(err) {
+                return next(err);
+            }
             res.format({
                 html: function() {
                     res.render('produtos/lista', {lista:results});
@@ -32,31 +35,31 @@ module.exports = function(app) {
     });
 
     app.get('/produtos/form', function(req, res) {
-        res.render('produtos/form',{errosValidacao:{}, produto:{}});
+        res.render('produtos/form', {errosValidacao:{}, produto:{}});
     });
 
     app.post('/produtos', function(req, res) {
         var produto = req.body;
-        console.log(produto);
+
+        req.assert('titulo', 'Titulo é obrigatório').notEmpty();
+        req.assert('preco', 'Formato inválido').isFloat();
+
+        var erros = req.validationErrors();
+        if(erros) {
+            res.format({
+                html: function() {
+                    res.status(400).render('produtos/form', {errosValidacao:erros, produto:produto});
+                },
+                json: function() {
+                    res.status(400).json(erros);
+                }
+            });
+
+            return;
+        }
 
         var connection = app.infra.connectionFactory();
         var produtosDAO = new app.infra.ProdutosDAO(connection);
-
-        var validadorTitulo = req.assert('titulo','Titulo deve ser preenchido').notEmpty();
-        req.assert('preco','Preço deve ser um número').isFloat();
-
-        var errors = req.validationErrors();
-        if(errors) {
-            res.format({
-                html: function() {
-                    res.status(400).render('produtos/form',{errosValidacao : errors, produto : produto});
-                },
-                json: function() {
-                    res.status(400).send(errors);
-                }
-            });
-            return;
-        }
 
         produtosDAO.salva(produto, function(err, results) {
             res.redirect('/produtos');
--- config/express.js
+++ config/express.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var load = require('express-load');
 var bodyParser = require('body-parser');
-
 var expressValidator = require('express-validator');
 
 module.exports = function() {
@@ -12,7 +11,6 @@ module.exports = function() {
 
     app.use(bodyParser.urlencoded({extended: true}));
     app.use(bodyParser.json());
-
     app.use(expressValidator());
 
     load('routes', {cwd: 'app'})
